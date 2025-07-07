4494f5e05193e77704a9fbf136d4e302c44039b3
giuseppe d'alessio <dalgius70@gmail.com>
2025-07-07 19:23:34 +0000

    [21:21:58.489] Running build in Washington, D.C., USA (East) – iad1 [21:

--- src/next.config.ts
+++ src/next.config.ts
@@ -12,9 +12,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    allowedDevOrigins: ['*.cloudworkstations.dev'],
-  },
+  allowedDevOrigins: ['*.cloudworkstations.dev'],
 };
 
 export default nextConfig;
